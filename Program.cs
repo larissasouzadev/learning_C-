a11d78e6ff32f7578526af209f3c70771a3c9ce7
larissa <larissacsouza585@gmail.com>
2025-07-03 11:55:28 -0300

    whiting hello world

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("HOla hermanos!");
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
--- obj/Debug/net9.0/learning_C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/learning_C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learning_C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f48cd372205f01be7806ae619ff9aec5f811228")]
 [assembly: System.Reflection.AssemblyProductAttribute("learning_C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learning_C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
