44f3bda822073cd3d39cc69826baa682018d5fe2
larissa <larissacsouza585@gmail.com>
2025-07-11 18:21:14 -0300

    feature loops

--- Program.cs
+++ Program.cs
@@ -141,15 +141,86 @@ using learning_c_.models;
 
 
 
-Calculadora calc = new Calculadora();
+// Calculadora calc = new Calculadora();
 
-calc.Somar(10, 50);
-calc.dividir(80, 40);
-calc.RaizQuadrada(256);
-calc.Cos(185);
-calc.Seno(90);
-calc.Subitrair(250, 145);
-calc.Tan(65);
+// calc.Somar(10, 50);
+// calc.dividir(80, 40);
+// calc.RaizQuadrada(256);
+// calc.Cos(185);
+// calc.Seno(90);
+// calc.Subitrair(250, 145);
+// calc.Tan(65);
+// // 
+
+// int num = 10;
+// Console.WriteLine(num);
+// Console.WriteLine("incrementando o 10");
+// num = num + 1;
+// Console.WriteLine(num);
+
+
+
+// estrutura de repetiçoes for(var;condicao;incremento)
+
+
+// int num = 5;
+
+// for (int contador = 0; contador <= 70; contador++)
+// {
+//     Console.WriteLine($"{num} x {contador} = {num * contador}");
+// }
+
+// estrutura de repetição while(condição)
+
+
+// int num = 6;
+// int contador = 0;
+
+// while (contador <= 20)
+// {
+//     Console.WriteLine($"{num} x {contador} = {num * contador}");
+//     contador++;
+
+// }   
+
+
+
+// menu interativo
+string opcao;
+while (true)
+{
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1- Cadastrar cliente");
+    Console.WriteLine("2- buscar cliente");
+    Console.WriteLine("3- apagar cliente");
+    Console.WriteLine("4- encerrar");
+    opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("cadastro de cliente");
+             break; 
+
+        case "2":
+            Console.WriteLine("busca de cliente");
+             break; 
+        case "3":
+            Console.WriteLine("apagar de cliente");
+             break; 
+        case "4":
+            Console.WriteLine("encerrar");
+            Environment.Exit(0);
+            break;
+
+
+        default:
+            Console.WriteLine("opção invalida");
+            break;
+        
+            }
+
+
+}
 
 
 
@@ -159,10 +230,33 @@ calc.Tan(65);
 
 
 
-// 
 
-int num = 10;
-Console.WriteLine(num);
-Console.WriteLine("incrementando o 10");
-num = num + 1;
-Console.WriteLine(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// do while 
+
+
+// int soma = 7, num = 5; // Agora 'soma' está corretamente inicializada
+
+// do
+// {
+//     Console.WriteLine("Digite um número, (0 para parar):");
+//     num = Convert.ToInt32(Console.ReadLine());
+//     soma += num;
+
+// } while (num != 0);
+
+// Console.WriteLine($"Total da soma dos números digitados é : {soma}");
